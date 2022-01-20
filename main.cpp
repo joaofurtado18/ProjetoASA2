827e9c761121a6816a05fc61b960f8f5a4eea61a
Carabalone <guilherme.caraba@gmail0.com>
2022-01-20 15:49:10 +0000

    mais erros

--- main.cpp
+++ main.cpp
@@ -9,23 +9,26 @@
 using namespace std;
 
 //compilation: g++ -std=c++11 -O3 -Wall main.cpp -lm
+// teste1 = 2 4
+// teste2 = 2 
+// teste3 = -
+stack<int> s;
 
-stack<int> dfs(int vertex, int size, int** m, bool visited[], stack<int> stack){
+void dfs(int vertex, int size, int** m, bool visited[], stack<int> stack){
     visited[vertex-1] = true;
     cout << vertex << " ";
     for(int i = 0; i < size; i++){
         if(m[vertex-1][i] == 1 && !visited[i]){
-            cout << "dfs " << i + 1 << endl;
-            stack = dfs(i+1, size, m, visited, stack);
-            stack.push(vertex);
+            dfs(i+1, size, m, visited, stack);
         }
     }
-    return stack;
+    s.push(vertex);
+    //return stack;
 }
 
 void dfs_transpose(int vertex, int size, int** m, bool visited[]){
     visited[vertex-1] = true;
-    cout << vertex << " ";
+    cout << "transpose "<<vertex << " ";
     for(int i = 0; i < size; i++){
         if(m[i][vertex-1] == 1 && !visited[i]){
             dfs_transpose(i+1, size, m, visited);
@@ -63,16 +66,18 @@ int main(){
         m[b1-1][b2-1] = 1;
     }
 
-    stack = dfs(1, vertices, m, visited, stack);
+    dfs(1, vertices, m, visited, stack);
+    s.push(1);
+    cout << endl;
     memset(visited, 0, sizeof(bool)*vertices);
     int numComponents = 0;
 
-    while (!stack.empty()){
-        int vertex2 = stack.top();
-        stack.pop();
+    while (!s.empty()){
+        int vertex2 = s.top();
+        cout << "stack" << vertex2 << endl;
+        s.pop();
         if (!visited[vertex2-1]){
             printf("Component %d: ", numComponents);
-            cout << endl << "vertex2 " << vertex2 << endl;
             dfs_transpose(vertex2, vertices, m, visited);
             numComponents++;
             printf("\n");
