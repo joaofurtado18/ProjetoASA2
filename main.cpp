67aceb4fd16166eb3d93fd1612ac93aec4e1488c
Carabalone <guilherme.caraba@gmail.com>
2022-01-23 10:36:17 -0300

    global variables

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 // teste3 = -
 
 int *count_vec;
+bool *visited, *recStack;
 
 bool isCyclicAux(int j, int vertices, bool visited[], bool recStack[], int **m)
 {
@@ -75,9 +76,8 @@ int main()
         return 0;
     }
 
-    bool visited[vertices];
-    bool visited2[vertices];
-    bool recStack[vertices];
+
+    // matrix filling
     int **m = new int *[vertices];
 
     for (int i = 0; i < vertices; i++)
@@ -85,9 +85,11 @@ int main()
         m[i] = new int[vertices];
         memset(m[i], 0, sizeof(int) * vertices);
     }
+
+    // visited and recStack
+    visited = new bool[vertices];
+    recStack = new bool[vertices];
     memset(visited, 0, sizeof(bool) * vertices);
-    count_vec = new int[vertices];
-    memset(count_vec, 0, sizeof(int) * vertices);
 
     for (int i = 0; i < archs; i++)
     {
@@ -98,14 +100,25 @@ int main()
         }
         m[b1 - 1][b2 - 1] = 1;
     }
-    if (is_cyclic(visited2, recStack, vertices, m))
-        cout << "0" << endl;
+    if (is_cyclic(visited, recStack, vertices, m))
+        // cout << "0" << endl;
+    // freeing memory;
+    delete recStack;
 
-    // cout << "dfs 1 ";
-    dfs_transpose(v1, vertices, m, visited);
-    // cout << "dfs 2 ";
+    count_vec = new int[vertices];
+    memset(count_vec, 0, sizeof(int) * vertices);
+    
+    //reseting visited
     memset(visited, 0, sizeof(bool) * vertices);
+
+    dfs_transpose(v1, vertices, m, visited);
+
+    //reseting visited again
+    memset(visited, 0, sizeof(bool) * vertices);
+
     dfs_transpose(v2, vertices, m, visited);
+    
+
     for (int i = 0; i < vertices; i++)
     {
         if (count_vec[i] > 1)
