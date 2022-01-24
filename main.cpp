bfc227b0601d5db6be30732042fedcc5915876b9
joaofurtado18 <joao.melo.furtado@tecnico.ulisboa.pt>
2022-01-24 19:09:55 -0100

    Ciclos a funcionar

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <algorithm>
 #include <cstring>
-#include<map>
+#include <map>
 
 using namespace std;
 
@@ -16,18 +16,50 @@ using namespace std;
 int *count_vec;
 bool *visited;
 
-void dfs(int vertex,  map<int, vector<int>> graph){
+void dfs(int vertex, map<int, vector<int>> graph)
+{
     visited[vertex - 1] = true;
     count_vec[vertex - 1] += 1;
     int size = graph[vertex].size();
     for (int i = 0; i < size; i++)
-        if (!visited[graph[vertex][i]-1])
+        if (!visited[graph[vertex][i] - 1])
             dfs(graph[vertex][i], graph);
+}
+
+bool is_cyclic_aux(int u, int *count_vec, int vertices, map<int, vector<int>> graph)
+{
+    count_vec[u] = 1;
+
+    int size = graph[u].size();
+    for (int i = 0; i < size; i++)
+    {
+        int v = graph[u][i];
+
+        if (count_vec[v] == 1)
+            return true;
+
+        if (count_vec[v] == 0 && is_cyclic_aux(v, count_vec, vertices, graph))
+            return true;
+    }
+
+    count_vec[u] = 2;
+    return false;
+}
+
+bool is_cyclic(int *count_vec, int vertices, map<int, vector<int>> graph)
+{
+    for (int i = 0; i < vertices; i++)
+        if (count_vec[i] == 0)
+            if (is_cyclic_aux(i, count_vec, vertices, graph))
+                return true;
+
+    return false;
 }
 
 // v1 -> v2
 // v1 = origin, v2 = destination
- map<int, vector<int>> addEdge(int v1, int v2, map<int, vector<int>> graph){
+map<int, vector<int>> addEdge(int v1, int v2, map<int, vector<int>> graph)
+{
     graph[v1].push_back(v2);
     return graph;
 }
@@ -36,7 +68,8 @@ void printGraph(map<int, vector<int>> graph)
 {
     map<int, vector<int>>::iterator it;
 
-    for (it = graph.begin(); it != graph.end(); it++){
+    for (it = graph.begin(); it != graph.end(); it++)
+    {
 
         cout << it->first;
         for (auto x : it->second)
@@ -46,33 +79,40 @@ void printGraph(map<int, vector<int>> graph)
     cout << endl;
 }
 
-void deliver_results(vector<int> vec, map<int, vector<int>> graph){
+void deliver_results(vector<int> vec, map<int, vector<int>> graph)
+{
     const int size = vec.size();
     vector<int> aux_vec = vec;
     cout << "vec ";
-    for (int i = 0; i < size; i++){
-        cout << vec[i]+1 << " ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << vec[i] + 1 << " ";
     }
     cout << endl;
+
     vector<int>::iterator ptr;
     vector<int>::iterator ptr2;
-    for (ptr = vec.begin(); ptr < vec.end(); ptr++){
-        cout <<"current: " << *ptr+1 << endl;
-        for (ptr2 = vec.begin(); ptr2 < vec.end(); ptr2++){
-            cout <<"teste " << *ptr2+1 << " ";
-            if (find(graph[*ptr+1].begin(), graph[*ptr+1].end(), *ptr2+1) != graph[*ptr+1].end()){
-                cout << endl<< "achou " << *ptr2+1 << " em " << *ptr+1;
+    for (ptr = vec.begin(); ptr < vec.end(); ptr++)
+    {
+        cout << "current: " << *ptr + 1 << endl;
+        for (ptr2 = vec.begin(); ptr2 < vec.end(); ptr2++)
+        {
+            cout << "teste " << *ptr2 + 1 << " ";
+            if (find(graph[*ptr + 1].begin(), graph[*ptr + 1].end(), *ptr2 + 1) != graph[*ptr + 1].end())
+            {
+                cout << endl
+                     << "achou " << *ptr2 + 1 << " em " << *ptr + 1;
                 vec.erase(ptr2);
             }
         }
         cout << endl;
     }
     cout << endl;
-    for(int i = 0; i < (int)vec.size(); i++){
+    for (int i = 0; i < (int)vec.size(); i++)
+    {
         cout << vec[i] + 1 << " ";
     }
 }
-
 
 int main()
 {
@@ -80,22 +120,25 @@ int main()
     vector<int> vec;
     map<int, vector<int>> graph;
 
-    if (scanf("%d %d ", &v1, &v2) == 0){
+    if (scanf("%d %d ", &v1, &v2) == 0)
+    {
         cout << "0" << endl;
         return 0;
     }
 
-    if (scanf("%d %d ", &vertices, &archs) == 0){
+    if (scanf("%d %d ", &vertices, &archs) == 0)
+    {
         cout << "0" << endl;
         return 0;
     }
 
     visited = new bool[vertices];
     memset(visited, 0, sizeof(bool) * vertices);
-
     // graph input
-    for (int i = 0; i < archs; i++){
-        if (scanf("%d %d ", &b1, &b2) == 0){
+    for (int i = 0; i < archs; i++)
+    {
+        if (scanf("%d %d ", &b1, &b2) == 0)
+        {
             cout << "0" << endl;
             return 0;
         }
@@ -104,7 +147,15 @@ int main()
 
     count_vec = new int[vertices];
     memset(count_vec, 0, sizeof(int) * vertices);
-    
+
+    if (is_cyclic(count_vec, vertices, graph))
+    {
+        cout << "0" << endl;
+        return 0;
+    }
+
+    memset(count_vec, 0, sizeof(int) * vertices);
+
     //reseting visited
     dfs(v1, graph);
     memset(visited, 0, sizeof(bool) * vertices);
@@ -114,20 +165,26 @@ int main()
 
     delete visited;
 
-    for (int i = 0; i < vertices; i++){
+    for (int i = 0; i < vertices; i++)
+    {
         cout << count_vec[i] << " ";
         if (count_vec[i] > 1)
             vec.push_back(i);
     }
     cout << endl;
 
-    if (vec.size() == 0){
+    if (vec.size() == 0)
+    {
         cout << "-" << endl;
         return 0;
     }
 
     deliver_results(vec, graph);
     cout << endl;
-    printGraph(graph);
+    //for (int i = 0; i < vertices; i++) {
+    //  cout << count_vec[i] << " count" << endl;
+    //}
+    //cout << endl;
+    //printGraph(graph);
     return 0;
 }
